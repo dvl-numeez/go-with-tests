a5a6cbb12aeb91a07753497b62edac3b797d9e65
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-02 11:27:43 +0530

    Chapter 1 Complete

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,34 @@
 package hello
 
 
-const helloPrefix = "Hello "
-func Hello(name string)string{
-	return helloPrefix+name
+
+const(
+	helloPrefix = "Hello "
+	spanishHelloPrefix = "Hola "
+	spanish = "Spanish"
+	french = "French"
+	frenchHelloPrefix = "Bonjour "
+
+
+)
+
+func Hello(name,language string)string{
+	if name==""{
+		return helloPrefix+"World"
+	}
+	prefix := greeting(language)
+	
+	return prefix+name
+}
+
+func greeting(language string)(prefix string){
+	switch language{
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix=helloPrefix
+	}
+	return
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,8 +4,30 @@ import "testing"
 
 
 func TestHello(t *testing.T){
-	actual:=Hello("Numeez")
+	actual:=Hello("Numeez","")
 	expected:="Hello Numeez"
+	assertCorrectMessage(t,actual,expected)
+	
+	t.Run("Say hello world when the empty string is passed",func(t *testing.T){
+		actual:=Hello("","")
+		expected:="Hello World"
+		assertCorrectMessage(t,actual,expected)
+	})
+	t.Run("Hello in Spanish",func(t *testing.T) {
+		actual:=Hello("Numeez","Spanish")
+		expected:="Hola Numeez"
+		assertCorrectMessage(t,actual,expected)
+	})
+	t.Run("Hello in French",func(t *testing.T) {
+		actual:=Hello("Numeez","French")
+		expected:="Bonjour Numeez"
+		assertCorrectMessage(t,actual,expected)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB,actual,expected string){
+	t.Helper()
 	if actual!=expected{
 		t.Errorf("Actual : %q Expected : %q",actual,expected)
 	}
