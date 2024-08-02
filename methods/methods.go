fc4ddadfe9f7caa77b75190a7915ef996406fcf8
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-02 15:44:13 +0530

    Adding more methods to the reactangle struct

--- methods/methods.go
+++ methods/methods.go
@@ -7,3 +7,6 @@ type Rectangle struct {
 func(rectangle Rectangle) Perimeter()float64{
 	return 2*(rectangle.Width+rectangle.Height)
 }
+func(rectangle Rectangle) Area()float64{
+	return rectangle.Width*rectangle.Height
+}
--- methods/methods_test.go
+++ methods/methods_test.go
@@ -14,3 +14,18 @@ func TestPermieter(t *testing.T){
 	}
 
 }
+
+
+func TestArea(t *testing.T){
+	t.Run("Rectangles",func(t *testing.T){
+		rectangle:= Rectangle{
+			Width: 12.0,
+			Height: 6.0,
+		}
+		actual:=rectangle.Area()
+		wanted:=72.0
+		if actual!=wanted{
+			t.Errorf("Actual %.2f Expected %.2f",actual,wanted)
+		}
+	})
+}
