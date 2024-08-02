d562da8c9967fe112a72929bed1ca0022101c29c
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-02 14:36:39 +0530

    Refactored and Chapter 3 Completed

--- iteration/itertation_test.go
+++ iteration/itertation_test.go
@@ -10,3 +10,9 @@ func TestIteration(t *testing.T){
 		t.Errorf("Actual : %q Expected:%q",actual,expected)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+		for i:=0;i<b.N;i++{
+			Repeat("a")
+		}
+}
