c7792787146b3f3bd7f4518807d350e4404baaad
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-02 14:53:21 +0530

    Refactored the arrays and slice function

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -3,10 +3,22 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T){
-	nums:=[5]int{1,2,3,4,5}
+	t.Run("Collection of 5 numbers",func(t *testing.T){
+		nums:=[]int{1,2,3,4,5}
 	actual:=Sum(nums)
 	expected:=15
 	if actual!=expected{
 		t.Errorf("Actual : %d Expected : %d",actual,expected)
 	}
+	})
+	t.Run("Collection of number of any size",func(t *testing.T){
+		nums:=[]int{1,2,3}
+		actual:=Sum(nums)
+		expected:=6
+		if actual!=expected{
+			t.Errorf("Actual : %d Expected : %d",actual,expected)
+		}
+		})
+	
+	
 }
