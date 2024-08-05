a79c588fca262d95006612787aa2bddd668923f2
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-05 16:51:24 +0530

    Completed reflection It will take time and practice to wrap my head around this topic

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,6 +1,55 @@
 package reflection
 
+import "reflect"
+
 
 func walk(x interface{},fn func(input string)){
-	fn("I still can't believe people are not using Go")
+	val := getValue(x)
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice,reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _,key:= range val.MapKeys(){
+			walk(val.MapIndex(key).Interface(),fn)
+		}
+	case reflect.Chan:
+		for{
+			if v,ok:=val.Recv();ok{
+				walkValue(v)
+			}else{
+				break
+			}
+		}
+	case reflect.Func:
+		result:=val.Call(nil)
+		for _,res:=range result{
+			walkValue(res)
+		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{})reflect.Value{
+	val:=reflect.ValueOf(x)
+	if val.Kind()==reflect.Pointer{
+		val = val.Elem()
+	}
+	return val
+
 }
