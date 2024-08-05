5a0de4be3a59210fbb514ea9979f678f8f50294f
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-05 14:53:52 +0530

    Done with the select

--- select/select_test.go
+++ select/select_test.go
@@ -9,20 +9,41 @@ import (
 
 
 func TestRacer(t *testing.T) {
-	slowServer:=httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20*time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-	fastServer:=httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	t.Run("comparing the speed of fast and slow url ",func(t *testing.T) {
+		slowServer:=makeDelayedServer(20*time.Millisecond)
+	fastServer:=makeDelayedServer(0*time.Millisecond)
 	
 	slowUrl:=slowServer.URL
 	fastUrl:=fastServer.URL
 	want := fastUrl
-	got := Racer(slowUrl, fastUrl)
+	got,_ := Racer(slowUrl, fastUrl)
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	})
+	t.Run("returns an error if it takes more than 10 seconds",func(t *testing.T){
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+	
+
+}
+
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
