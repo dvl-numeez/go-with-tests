c5b0c744e5e056b10c5f95b6d76bfcd31ae91d8f
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-07 13:05:29 +0530

    Completed Generics

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -14,43 +14,27 @@ func TestAssertFunctions(t *testing.T) {
 }
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
+		myStackOfInts := new(Stack[int])
+
 		AssertTrue(t, myStackOfInts.IsEmpty())
+
+	
 		myStackOfInts.Push(123)
 		AssertFalse(t, myStackOfInts.IsEmpty())
+
+
 		myStackOfInts.Push(456)
 		value, _ := myStackOfInts.Pop()
 		AssertEqual(t, value, 456)
 		value, _ = myStackOfInts.Pop()
 		AssertEqual(t, value, 123)
 		AssertTrue(t, myStackOfInts.IsEmpty())
-	})
 
-	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(StackOfStrings)
-		AssertTrue(t, myStackOfStrings.IsEmpty())
-		myStackOfStrings.Push("123")
-		AssertFalse(t, myStackOfStrings.IsEmpty())
-		myStackOfStrings.Push("456")
-		value, _ := myStackOfStrings.Pop()
-		AssertEqual(t, value, "456")
-		value, _ = myStackOfStrings.Pop()
-		AssertEqual(t, value, "123")
-		AssertTrue(t, myStackOfStrings.IsEmpty())
-	})
-	t.Run("interface stack DX is horrid", func(t *testing.T) {
-		myStackOfInts := new(StackOfInts)
-	
 		myStackOfInts.Push(1)
 		myStackOfInts.Push(2)
 		firstNum, _ := myStackOfInts.Pop()
 		secondNum, _ := myStackOfInts.Pop()
-		reallyFirstNum, ok := firstNum.(int)
-		AssertTrue(t, ok)
-		reallySecondNum, ok := secondNum.(int)
-		AssertTrue(t, ok)
-
-		AssertEqual(t, reallyFirstNum+reallySecondNum, 3)
+		AssertEqual(t, firstNum+secondNum, 3)
 	})
 }
 
