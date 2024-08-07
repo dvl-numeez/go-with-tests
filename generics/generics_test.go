64d35aad48bd572f46533a941970a9e84f5471b2
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-07 12:47:08 +0530

    Using interface for more abstraction

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -2,30 +2,82 @@ package generics
 
 import "testing"
 
-
-
-func TestAssertFunctions(t *testing.T){
-	t.Run("Asserting functions",func(t *testing.T){
-		AssertEqual(t,1,1)
-		AssertNotEqual(t,1,2)
+func TestAssertFunctions(t *testing.T) {
+	t.Run("Asserting functions", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
 	})
-	t.Run("assert on strings",func(t *testing.T){
+	t.Run("assert on strings", func(t *testing.T) {
 		AssertEqual(t, "hello", "hello")
-	AssertNotEqual(t, "hello", "Numeez")
+		AssertNotEqual(t, "hello", "Numeez")
+	})
+}
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := new(StackOfStrings)
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+		myStackOfStrings.Push("456")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "456")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "123")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+	t.Run("interface stack DX is horrid", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+	
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		reallyFirstNum, ok := firstNum.(int)
+		AssertTrue(t, ok)
+		reallySecondNum, ok := secondNum.(int)
+		AssertTrue(t, ok)
+
+		AssertEqual(t, reallyFirstNum+reallySecondNum, 3)
 	})
 }
 
-func AssertEqual[T comparable](t *testing.T,got,want T){
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
 
-
-func AssertNotEqual[T comparable](t *testing.T,got,want T){
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
-		t.Errorf("didn't want %d", got)
+		t.Errorf("didn't want %v", got)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
 	}
 }
