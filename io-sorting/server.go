4bc5b35157245608b738d34d185088fbdc13c88f
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-08 13:20:03 +0530

    Getting started with io and sorting

---
+++ io-sorting/server.go
@@ -0,0 +1,136 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+)
+
+const jsonContentType = "application/json"
+
+type Player struct{
+	Name string
+	Wins int
+}
+
+type PlayerStore interface{
+	GetPlayerScore(name string)int
+	RecordWin(name string)
+	GetLeague() []Player
+}
+type PlayerServer struct{
+	Store PlayerStore
+	http.Handler
+}
+
+type StubPlayerStore struct{
+	scores   map[string]int
+	winCalls []string
+	league   []Player
+}
+
+type FileSystemPlayerStore struct{
+	database io.ReadSeeker
+}
+
+func(s *FileSystemPlayerStore)GetLeague() []Player{
+	s.database.Seek(0,io.SeekStart)
+	player,_:=NewLeague(s.database)
+	return player
+}
+
+func(store *StubPlayerStore)GetLeague()[]Player{
+	return store.league
+}
+
+func(store *StubPlayerStore)GetPlayerScore(name string)int{
+	return store.scores[name]
+}
+func (store *StubPlayerStore)RecordWin(name string){
+	store.winCalls = append(store.winCalls, name)
+}
+
+func NewPlayerServer(store PlayerStore)*PlayerServer{
+	p := new(PlayerServer)
+
+	p.Store = store
+
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+
+	p.Handler = router
+
+	return p
+	
+}
+
+func (s *PlayerServer)ServeHTTP(w http.ResponseWriter, r *http.Request){
+	
+	s.Handler.ServeHTTP(w,r)
+	
+	
+}
+
+func (p *PlayerServer)leagueHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-type", jsonContentType)
+	json.NewEncoder(w).Encode(p.Store.GetLeague())
+	w.WriteHeader(http.StatusOK)
+}
+func (p *PlayerServer)playersHandler(w http.ResponseWriter, r *http.Request){
+	method:=r.Method
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	switch method{
+	case http.MethodGet:
+		p.showScore(w,player)
+	case http.MethodPost:
+		p.processWin(w,player)
+		
+
+	}
+}
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+
+	score := p.Store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter,player string) {
+	p.Store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
+func (p *PlayerServer)getLeagueTable()[]Player{
+	return []Player{
+		{"Chris", 20},
+	}
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
+}
+
+func NewLeague(rdr io.Reader) ([]Player, error) {
+	var league []Player
+	err := json.NewDecoder(rdr).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("problem parsing league, %v", err)
+	}
+
+	return league, err
+}
---
+++ io-sorting/server_test.go
@@ -0,0 +1,90 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+
+func TestFileSystemStore(t *testing.T) {
+
+	t.Run("league from a reader", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"Cleo", 10},
+			{"Chris", 33},
+		}
+		got = store.GetLeague()
+		assertLeague(t, got, want)
+		assertLeague(t, got, want)
+	})
+}
+
+func AssertResponseBody(t testing.TB,got,want string){
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+	
+}
+
+func NewGetScoreRequest(name string)*http.Request{
+	url:=fmt.Sprintf("/players/%s",name)
+	req,_:= http.NewRequest(http.MethodGet, url, nil)
+	return req
+}
+func NewPostRequest(name string)*http.Request{
+	url:=fmt.Sprintf("/players/%s",name)
+	req,_:=http.NewRequest(http.MethodPost, url, nil)
+	return req
+}
+
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
+}
+
+func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
+	t.Helper()
+	err := json.NewDecoder(body).Decode(&league)
+
+	if err != nil {
+		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
+	}
+
+	return
+}
+
+func assertLeague(t testing.TB, got, want []Player) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func newLeagueRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/league", nil)
+	return req
+}
+
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
+	}
+}
